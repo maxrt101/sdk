0f269bc56dba7106d5cf687d3fabcf43b7b2f042
maxrt <max.r.tkachuk@gmail.com>
2025-04-18 07:15:37 +0300

    [LIB] Add software watchdog (counter)

---
+++ lib/swdg/soft_wdg.c
@@ -0,0 +1,62 @@
+/** ========================================================================= *
+*
+ * @file soft_wdg.c
+ * @date 18-04-2025
+ * @author Maksym Tkachuk <max.r.tkachuk@gmail.com>
+ *
+ *  ========================================================================= */
+
+/* Includes ================================================================= */
+#include "swdg/soft_wdg.h"
+#include "error/assertion.h"
+#include "log/log.h"
+#include "os/reset/reset.h"
+
+/* Defines ================================================================== */
+#define LOG_TAG SWDG
+
+/* Macros =================================================================== */
+/* Exposed macros =========================================================== */
+/* Enums ==================================================================== */
+/* Types ==================================================================== */
+/* Variables ================================================================ */
+/* Private functions ======================================================== */
+/* Shared functions ========================================================= */
+void soft_wdg_init(soft_wdg_t * wdg, uint32_t max, uint8_t action, const char * label) {
+  ASSERT_RETURN(wdg);
+
+  wdg->label = label;
+  wdg->max = max;
+  wdg->action = action;
+  wdg->counter = 0;
+}
+
+void soft_wdg_inc(soft_wdg_t * wdg) {
+  ASSERT_RETURN(wdg);
+
+  if (wdg->counter >= wdg->max) {
+    if (wdg->action & SWDG_ACTION_LOG) {
+      log_warn(wdg->label ? "Timeout expired for %s" : "Timeout expired", wdg->label);
+    }
+
+    if (wdg->action & SWDG_ACTION_NOTIFY) {
+      soft_wdg_on_timeout(wdg);
+    }
+
+    if (wdg->action & SWDG_ACTION_REBOOT_WDT) {
+      os_reset(OS_RESET_WDG);
+    }
+
+    if (wdg->action & SWDG_ACTION_REBOOT_SOFT) {
+      os_reset(OS_RESET_SOFT);
+    }
+
+    if (wdg->action & SWDG_ACTION_REBOOT_HARD) {
+      os_reset(OS_RESET_HARD);
+    }
+  }
+}
+
+__WEAK void soft_wdg_on_timeout(soft_wdg_t * wdg) {
+  // Does nothing
+}
---
+++ lib/swdg/soft_wdg.h
@@ -0,0 +1,70 @@
+/** ========================================================================= *
+*
+ * @file soft_wdg.h
+ * @date 18-04-2025
+ * @author Maksym Tkachuk <max.r.tkachuk@gmail.com>
+ *
+ * @brief Software WatchDog
+ *
+ *  ========================================================================= */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ================================================================= */
+#include "time/timeout.h"
+
+/* Defines ================================================================== */
+/* Macros =================================================================== */
+/* Enums ==================================================================== */
+typedef enum {
+  SWDG_ACTION_REBOOT_SOFT  = (1 << 0),
+  SWDG_ACTION_REBOOT_HARD  = (1 << 1),
+  SWDG_ACTION_REBOOT_WDT   = (1 << 2),
+  SWDG_ACTION_NOTIFY       = (1 << 3),
+  SWDG_ACTION_LOG          = (1 << 4),
+
+  SWDT_ACTION_DEFAULT      = SWDG_ACTION_REBOOT_WDT | SWDG_ACTION_NOTIFY | SWDG_ACTION_LOG,
+} soft_wdg_action_t;
+
+/* Types ==================================================================== */
+/**
+ * Software WatchDog Context
+ */
+typedef struct {
+  const char * label;
+  uint32_t counter;
+  uint32_t max;
+  uint8_t action;
+} soft_wdg_t;
+
+/* Variables ================================================================ */
+/* Shared functions ========================================================= */
+/**
+ * Init WatchDogTimer
+ *
+ * @param wdg SoftWDG Context
+ * @param max Max value, that counter can reach before being considered expired
+ * @param action Bitmask of actions to perform
+ */
+void soft_wdg_init(soft_wdg_t * wdg, uint32_t max, uint8_t action, const char * label);
+
+/**
+ * Update WatchDog counter
+ *
+ * @param wdg SoftWDG Context
+ */
+void soft_wdg_inc(soft_wdg_t * wdg);
+
+/**
+ * Called when counter expired
+ *
+ * @param wdg SoftWDG Context
+ */
+void soft_wdg_on_timeout(soft_wdg_t * wdg);
+
+#ifdef __cplusplus
+}
+#endif
