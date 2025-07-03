8e12b317159b8d7dbd2014025c7bc51ac6b6afbf
maxrt <max.r.tkachuk@gmail.com>
2025-07-03 04:01:27 +0300

    Add feature support

--- toolchain/utils.cmake
+++ toolchain/utils.cmake
@@ -10,6 +10,16 @@
 
 include_guard(GLOBAL)
 
+string(ASCII 27 ESC)
+set(COLOR_RESET     "${ESC}[m")
+set(COLOR_RED       "${ESC}[31m")
+set(COLOR_GREEN     "${ESC}[32m")
+set(COLOR_YELLOW    "${ESC}[33m")
+set(COLOR_BLUE      "${ESC}[34m")
+set(COLOR_MAGENTA   "${ESC}[35m")
+set(COLOR_CYAN      "${ESC}[36m")
+set(COLOR_WHITE     "${Esc}[37m")
+
 #
 # @brief Retrieves host OS type (Windows, Darwin or Linux)
 #
@@ -30,7 +40,7 @@ endfunction()
 #
 # @brief Retrieves full path to executable
 #
-# @param[in] bin_name Name of the executable
+# @param[in]  bin_name Name of the executable
 # @param[out] out_path Where to put resulting path
 #
 macro(bin_which bin_name out_path)
@@ -43,7 +53,7 @@ endmacro()
 #
 # @brief Crops filename out of a path
 #
-# @param[in] file_path Path to file
+# @param[in]  file_path Path to file
 # @param[out] out_path Where to put resulting path
 #
 macro(dir_name file_path out_path)
@@ -51,4 +61,54 @@ macro(dir_name file_path out_path)
             OUTPUT_VARIABLE ${out_path}
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
+endmacro()
+
+#
+# @brief Parse name and value from feature declaration (NAME=VALUE)
+#
+# @param[in]  Feature declaration
+# @param[out] Parsed feature name
+# @param[out] Parsed feature value
+#
+macro(feature_parse_name_value feature name value)
+    string(REPLACE "=" ";" feature_tokens ${feature})
+    list(GET feature_tokens 0 ${name})
+    list(GET feature_tokens 1 ${value})
+endmacro()
+
+#
+# @brief Get feature value. Check CMake variables and ENV variables
+#        if not defined - will use default value
+#
+# @param[in]  Feature declaration
+# @param[out] Feature value
+#
+macro(feature_get_value feature value)
+    if (DEFINED ${feature})
+        set(${value} ${${feature}})
+    elseif (DEFINED ENV{${feature}})
+        set(${value} $ENV{${feature}})
+    else()
+        set(${value} UNDEFINED)
+    endif ()
+endmacro()
+
+#
+# @brief Parse all features in ${features}, adding definitions to compilation defines
+#
+# @param[in] features List of NAME=VALUE pairs
+#
+macro (feature_parse_all)
+    foreach (feature ${ARGN})
+        feature_parse_name_value(${feature} name default)
+        feature_get_value(${name} val)
+
+        if (${val} STREQUAL UNDEFINED)
+            message(STATUS "${COLOR_BLUE}Feature ${name} using default value ${default}${COLOR_RESET}")
+            project_add_define(ALL ${feature})
+        else ()
+            message(STATUS "${COLOR_YELLOW}Feature ${name} overridden, value ${val}${COLOR_RESET}")
+            project_add_define(ALL ${name}=${val})
+        endif ()
+    endforeach ()
 endmacro()
