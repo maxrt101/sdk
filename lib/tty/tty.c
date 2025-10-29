ba862cba75496405ab34e33a25415bcf9905f200
maxrt <max.r.tkachuk@gmail.com>
2025-10-29 04:17:54 +0200

    [LIB] Implement ANSI escape sequence handling & moving cursor with arrows within the line

--- lib/tty/tty.c
+++ lib/tty/tty.c
@@ -10,7 +10,9 @@
 
 /* Includes ================================================================= */
 #include "tty/tty.h"
+#include "ansi.h"
 #include "error/assertion.h"
+#include <stdio.h>
 
 /* Defines ================================================================== */
 /* Macros =================================================================== */
@@ -19,6 +21,172 @@
 /* Types ==================================================================== */
 /* Variables ================================================================ */
 /* Private functions ======================================================== */
+static error_t tty_process_ansi_esc(__UNUSED tty_t * tty, tty_line_t * line, char c) {
+  switch (c) {
+    case '[': line->input.state = TTY_LINE_INPUT_STATE_CSI;     break;
+    case 'P': line->input.state = TTY_LINE_INPUT_STATE_DCS;     break;
+    case ']': line->input.state = TTY_LINE_INPUT_STATE_OSC;     break;
+    default:  line->input.state = TTY_LINE_INPUT_STATE_DEFAULT; break;
+  }
+  return E_AGAIN;
+}
+
+static error_t tty_process_ansi_csi(tty_t * tty, tty_line_t * line, char c) {
+  switch (c) {
+    case 'C': // ESC [ C - Move cursor right
+      if (line->input.cursor < line->size) {
+        line->input.cursor++;
+        tty_write_fmt(tty, ANSI_CURSOR_MOVE_RIGHT());
+      }
+      break;
+
+    case 'D': // ESC [ D - Move cursor left
+      if (line->input.cursor > 0) {
+        line->input.cursor--;
+        tty_write_fmt(tty, ANSI_CURSOR_MOVE_LEFT());
+      }
+      break;
+
+    default:
+      return tty_process_ansi_csi_custom(tty, line, c);
+  }
+
+  line->input.state = TTY_LINE_INPUT_STATE_DEFAULT;
+
+  return E_AGAIN;
+}
+
+static error_t tty_process_ansi_dcs(tty_t * tty, tty_line_t * line, char c) {
+  return tty_process_ansi_dcs_custom(tty, line, c);
+}
+
+static error_t tty_process_ansi_osc(tty_t * tty, tty_line_t * line, char c) {
+  return tty_process_ansi_osc_custom(tty, line, c);
+}
+
+static error_t tty_process_ansi(tty_t * tty, tty_line_t * line, char c) {
+  if (c == TTY_ASCII_KEY_ESC) {
+    line->input.state = TTY_LINE_INPUT_STATE_ESC;
+    return E_AGAIN;
+  }
+
+  switch (line->input.state) {
+    case TTY_LINE_INPUT_STATE_DEFAULT: return E_OK;
+    case TTY_LINE_INPUT_STATE_ESC:     return tty_process_ansi_esc(tty, line, c);
+    case TTY_LINE_INPUT_STATE_CSI:     return tty_process_ansi_csi(tty, line, c);
+    case TTY_LINE_INPUT_STATE_DCS:     return tty_process_ansi_dcs(tty, line, c);
+    case TTY_LINE_INPUT_STATE_OSC:     return tty_process_ansi_osc(tty, line, c);
+    default:
+      line->input.state = TTY_LINE_INPUT_STATE_DEFAULT;
+      break;
+  }
+
+  return E_OK;
+}
+
+static error_t tty_process_char(tty_t * tty, tty_line_t * line, char c) {
+  ERROR_CHECK_RETURN(tty_process_ansi(tty, line, c));
+
+  // If char is backspace
+  if (c == TTY_ASCII_KEY_BACKSPACE) {
+    // If string buffer is not empty
+    if (line->size > 0 && line->input.cursor > 0) {
+      if (line->input.cursor != line->size) {
+        // If not at the end of the line (hard case)
+        // Move the remainder of the line from char preceding cursor
+        // (which will get erased) to the end of the line
+        memcpy(
+          &line->buf[line->input.cursor-1],
+          &line->buf[line->input.cursor],
+          line->size - line->input.cursor + 1
+        );
+
+        // Move cursor to the left & reduce size by 1
+        line->size         -= 1;
+        line->input.cursor -= 1;
+
+
+        // Reprint remainder of line (from cursor to the end) and move
+        // cursor back to where it belongs
+        if (tty_get_flag(tty, TTY_FLAG_ECHO_INPUT)) {
+          size_t left = line->size - line->input.cursor;
+
+          tty_write_fmt(
+            tty,
+            "\b%.*s " ANSI_CURSOR_MOVE_LEFT_FMT,
+            left, &line->buf[line->input.cursor], left + 1
+          );
+        }
+      } else {
+        // If at the end of the line (easy case)
+        // Send backspace, which will move the cursor left by 1 char, then
+        // send blank space to overwrite the symbol and another backspace
+        // to put cursor 1 symbol back
+        if (tty_get_flag(tty, TTY_FLAG_ECHO_INPUT)) {
+          char buf[3] = {c, ' ', c};
+          vfs_write(tty->file, (uint8_t *) buf, 3);
+        }
+
+        // Move cursor to the left & reduce size by 1
+        line->size         -= 1;
+        line->input.cursor -= 1;
+      }
+    }
+    // If there is nothing to erase, skip
+    return E_AGAIN;
+  }
+
+  // If char is enter (CR), send CR & LF back into the UART
+  if (c == TTY_ASCII_KEY_ENTER) {
+    if (tty_get_flag(tty, TTY_FLAG_ECHO_INPUT)) {
+      char buf[2] = {'\r', '\n'};
+      vfs_write(tty->file, (uint8_t *) buf, 2);
+    }
+
+    line->size += 1;
+
+    // Terminate the buffer
+    line->buf[line->size-1] = '\0';
+    return E_OK;
+  }
+
+  if (line->input.cursor != line->size) {
+    // If cursor is not at the end of the line (hard case)
+    // Move remainder chars from the end to the cursor to the left
+    for (ssize_t i = line->size - line->input.cursor + 1; i >= 0; --i) {
+      line->buf[line->input.cursor + i + 1] = line->buf[line->input.cursor + i];
+    }
+
+    // Save char at cursor position
+    line->buf[line->input.cursor++] = c;
+    line->size++;
+
+    if (tty_get_flag(tty, TTY_FLAG_ECHO_INPUT)) {
+      size_t left = line->size - line->input.cursor;
+
+      // Print new char + remainder of line (from cursor to the end) and move
+      // cursor back to where it belongs
+      tty_write_fmt(
+        tty,
+        "%.*s" ANSI_CURSOR_MOVE_LEFT_FMT,
+        left + 1, &line->buf[line->input.cursor-1], left
+      );
+    }
+  } else {
+    // If cursor is at the end of the line (easy case)
+    // Save char to the end of the buffer
+    line->buf[line->size++] = c;
+    line->input.cursor++;
+
+    // If input echoing is on, send received char back unto UART to display it
+    if (tty_get_flag(tty, TTY_FLAG_ECHO_INPUT)) {
+      vfs_write(tty->file, (uint8_t *) &c, 1);
+    }
+  }
+
+  return E_AGAIN;
+}
+
 /* Shared functions ========================================================= */
 error_t tty_init(tty_t * tty, vfs_file_t * file) {
   ASSERT_RETURN(tty && file, E_NULL);
@@ -35,10 +203,27 @@ error_t tty_reset(tty_t * tty) {
   return vfs_ioctl(tty->file, VFS_IOCTL_RESET_DEVICE);
 }
 
+error_t tty_line_reset(tty_line_t * line) {
+  ASSERT_RETURN(line, E_NULL);
+
+  line->size               = 0;
+  line->input.cursor       = 0;
+  line->input.state        = TTY_LINE_INPUT_STATE_DEFAULT;
+  line->input.custom_state = TTY_LINE_INPUT_CUSTOM_STATE_DEFAULT;
+
+  memset(line->buf, 0, sizeof(line->buf));
+
+  return E_OK;
+}
+
 error_t tty_line_from_str(tty_line_t * line, const char * str) {
   ASSERT_RETURN(line && str, E_NULL);
 
-  line->size = strlen(str);
+  line->input.state        = TTY_LINE_INPUT_STATE_DEFAULT;
+  line->input.custom_state = TTY_LINE_INPUT_CUSTOM_STATE_DEFAULT;
+  line->size               = strlen(str);
+  line->input.cursor       = line->size;
+
   memcpy(line->buf, str, line->size + 1);
 
   return E_OK;
@@ -47,7 +232,11 @@ error_t tty_line_from_str(tty_line_t * line, const char * str) {
 error_t tty_line_from_buf(tty_line_t * line, const char * buf, size_t size) {
   ASSERT_RETURN(line && buf, E_NULL);
 
-  line->size = size;
+  line->input.state        = TTY_LINE_INPUT_STATE_DEFAULT;
+  line->input.custom_state = TTY_LINE_INPUT_CUSTOM_STATE_DEFAULT;
+  line->size               = size;
+  line->input.cursor       = size;
+
   memcpy(line->buf, buf, size + 1);
 
   return E_OK;
@@ -75,6 +264,18 @@ bool tty_get_flag(tty_t * tty, tty_flag_t flag) {
   return tty->flags & flag ? 1 : 0;
 }
 
+error_t tty_get_char(tty_t * tty, char * ch) {
+  ASSERT_RETURN(tty && ch, E_NULL);
+
+  return vfs_read(tty->file, (uint8_t *) &ch, 1, VFS_READ_FLAG_NONE);
+}
+
+error_t tty_get_char_async(tty_t * tty, char * ch) {
+  ASSERT_RETURN(tty && ch, E_NULL);
+
+  return vfs_read(tty->file, (uint8_t *) &ch, 1, VFS_READ_FLAG_NOBLOCK);
+}
+
 error_t tty_read_line(tty_t * tty, tty_line_t * line) {
   ASSERT_RETURN(tty && line, E_NULL);
 
@@ -84,41 +285,10 @@ error_t tty_read_line(tty_t * tty, tty_line_t * line) {
     char c = '\0';
 
     // Read 1 char from UART
-    vfs_read(tty->file, (uint8_t *) &c, 1, VFS_READ_FLAG_NOBLOCK);
-
-    if (c == TTY_ASCII_KEY_ESC) {
-      continue;
-    }
-
-    // If char is backspace
-    if (c == TTY_ASCII_KEY_BACKSPACE) {
-      // If string buffer is not empty
-      if (line->size > 0) {
-        // Send backspace, which will move the cursor left by 1 char, then
-        // send blank space to overwrite the symbol and another backspace
-        // to put cursor 1 symbol back
-        char buf[3] = {c, ' ', c};
-        vfs_write(tty->file, (uint8_t *) buf, 3);
-        line->size -= 1;
+    if (vfs_read(tty->file, (uint8_t *) &c, 1, VFS_READ_FLAG_NONE) == E_OK) {
+      if (tty_process_char(tty, line, c) == E_OK) {
+        return E_OK;
       }
-      // If there is nothing to erase, skip
-      continue;
-    }
-
-    // If char is enter (CR), send CR & LF back into the UART
-    if (c == TTY_ASCII_KEY_ENTER) {
-      char buf[2] = {'\r', '\n'};
-      vfs_write(tty->file, (uint8_t *) buf, 2);
-      line->size += 1;
-      break;
-    }
-
-    // Save char to the buffer
-    line->buf[line->size++] = c;
-
-    // If input echoing is on, send received char back unto UART to display it
-    if (tty_get_flag(tty, TTY_FLAG_ECHO_INPUT)) {
-      vfs_write(tty->file, (uint8_t *) &c, 1);
     }
   }
 
@@ -138,58 +308,58 @@ error_t tty_read_line_async(tty_t * tty, tty_line_t * line) {
       return E_AGAIN;
     }
 
-    // Read 1 char from UART
     ERROR_CHECK_RETURN(vfs_read(tty->file, (uint8_t *) &c, 1, VFS_READ_FLAG_NONE));
 
-    if (c == TTY_ASCII_KEY_ESC) {
-      return E_AGAIN;
-    }
-
-    // If char is backspace
-    if (c == TTY_ASCII_KEY_BACKSPACE) {
-      // If string buffer is not empty
-      if (line->size > 0) {
-        // Send backspace, which will move the cursor left by 1 char, then
-        // send blank space to overwrite the symbol and another backspace
-        // to put cursor 1 symbol back
-        char buf[3] = {c, ' ', c};
-        vfs_write(tty->file, (uint8_t *) buf, 3);
-        line->size -= 1;
-      }
-      // If there is nothing to erase, skip
-      return E_AGAIN;
-    }
-
-    // If char is enter (CR), send CR & LF back into the UART
-    if (c == TTY_ASCII_KEY_ENTER) {
-      char buf[2] = {'\r', '\n'};
-      vfs_write(tty->file, (uint8_t *) buf, 2);
-      line->size += 1;
-
-      // Terminate the buffer
-      line->buf[line->size-1] = '\0';
-      return E_OK;
-    }
-
-    // Save char to the buffer
-    line->buf[line->size++] = c;
-
-    // If input echoing is on, send received char back unto UART to display it
-    if (tty_get_flag(tty, TTY_FLAG_ECHO_INPUT)) {
-      vfs_write(tty->file, (uint8_t *) &c, 1);
-    }
-
-    return E_AGAIN;
-  } else {
-    // Terminate the buffer
-    line->buf[line->size-1] = '\0';
-    return E_OK;
+    return tty_process_char(tty, line, c);
   }
+
+  // Terminate the buffer
+  line->buf[line->size-1] = '\0';
+
+  return E_OK;
 }
 
 error_t tty_write_line(tty_t * tty, tty_line_t * line) {
   ASSERT_RETURN(tty && line, E_NULL);
 
-  return vfs_write(tty->file, (uint8_t *) line->buf, line->size);
+  ERROR_CHECK_RETURN(vfs_write(tty->file, (uint8_t *) line->buf, line->size));
+
+  // If cursor is not at the end of line
+  if (line->input.cursor < line->size) {
+    // Move the cursor left, because whole line was just printed, and cursor is
+    // currently at the end of it
+    tty_write_fmt(tty, ANSI_CURSOR_MOVE_LEFT_FMT, line->size - line->input.cursor);
+  }
+
+  return E_OK;
 }
 
+error_t tty_write_fmt(tty_t * tty, const char * fmt, ...) {
+  ASSERT_RETURN(tty && fmt, E_NULL);
+
+  va_list args;
+  va_start(args, fmt);
+
+  char buffer[TTY_MAX_LINE_SIZE];
+  size_t size = vsnprintf(buffer, sizeof(buffer), fmt, args);
+  error_t result = vfs_write(tty->file, (uint8_t *) buffer, size);
+
+  va_end(args);
+
+  return result;
+}
+
+__WEAK error_t tty_process_ansi_csi_custom(__UNUSED tty_t * tty, tty_line_t * line, __UNUSED char c) {
+  line->input.state = TTY_LINE_INPUT_STATE_DEFAULT;
+  return E_AGAIN;
+}
+
+__WEAK error_t tty_process_ansi_dcs_custom(__UNUSED tty_t * tty, tty_line_t * line, __UNUSED char c) {
+  line->input.state = TTY_LINE_INPUT_STATE_DEFAULT;
+  return E_AGAIN;
+}
+
+__WEAK error_t tty_process_ansi_osc_custom(__UNUSED tty_t * tty, tty_line_t * line, __UNUSED char c) {
+  line->input.state = TTY_LINE_INPUT_STATE_DEFAULT;
+  return E_AGAIN;
+}
--- lib/tty/tty.h
+++ lib/tty/tty.h
@@ -59,11 +59,48 @@ typedef enum {
 
 /* Types ==================================================================== */
 /**
- * @return TTY Line (char buffer with size)
+ * @return TTY Line
  */
 typedef struct {
+  /** Line buffer */
   char buf[TTY_MAX_LINE_SIZE];
+
+  /** Line size */
   uint16_t size;
+
+  /** Input processing context (used by tty_read_line*) */
+  struct {
+    /** Cursor position within the like buffer */
+    uint16_t cursor;
+
+    /** Line input state. Used for processing ANSI escape sequences */
+    __PACKED_ENUM {
+      /** Normal operation */
+      TTY_LINE_INPUT_STATE_DEFAULT = 0,
+
+      /** ESC - escape ('\x1b') was encountered */
+      TTY_LINE_INPUT_STATE_ESC,
+
+      /** CSI - Control Sequence Introducer ('ESC [') was encountered */
+      TTY_LINE_INPUT_STATE_CSI,
+
+      /** DCS - Device Control String ('ESC P') was encountered */
+      TTY_LINE_INPUT_STATE_DCS,
+
+      /** OSC - Operating System Command ('ESC ]') was encountered */
+      TTY_LINE_INPUT_STATE_OSC,
+    } state;
+
+    __PACKED_ENUM {
+      /** Custom state used by tty_process_ansi_*_custom */
+      TTY_LINE_INPUT_CUSTOM_STATE_DEFAULT = 0,
+
+      /** Can be used to define additional states for tty_process_ansi_*_custom */
+#ifdef TTY_LINE_INPUT_CUSTOM_STATE
+      TTY_LINE_INPUT_CUSTOM_STATE
+#endif
+    } custom_state;
+  } input;
 } tty_line_t;
 
 /**
@@ -93,6 +130,13 @@ error_t tty_init(tty_t * tty, vfs_file_t * file);
  * @param tty TTY Context
  */
 error_t tty_reset(tty_t * tty);
+
+/**
+ * Reset TTY Line
+ *
+ * @param line TTY Line
+ */
+error_t tty_line_reset(tty_line_t * line);
 
 /**
  * Initialize TTY Line from c string
@@ -136,7 +180,23 @@ error_t tty_clear_flag(tty_t * tty, tty_flag_t flag);
 bool tty_get_flag(tty_t * tty, tty_flag_t flag);
 
 /**
- * Read line (blocking) (@ref tty_flag_t)
+ * Read 1 char from TTY input (blocking)
+ *
+ * @param tty TTY Context
+ * @param ch Where to put read char
+ */
+error_t tty_get_char(tty_t * tty, char * ch);
+
+/**
+ * Read 1 char from TTY input (async)
+ *
+ * @param tty TTY Context
+ * @param ch Where to put read char
+ */
+error_t tty_get_char_async(tty_t * tty, char * ch);
+
+/**
+ * Read line from TTY input (blocking) (@ref tty_flag_t)
  *
  * @param tty TTY Context
  * @param line Line buffer to raed into
@@ -144,9 +204,10 @@ bool tty_get_flag(tty_t * tty, tty_flag_t flag);
 error_t tty_read_line(tty_t * tty, tty_line_t * line);
 
 /**
- * Read line (async) (@ref tty_flag_t)
+ * Read line from TTY input (async) (@ref tty_flag_t)
  *
  * @note If no char is detected, will return with E_AGAIN
+ * @note When CR LF is received - will terminate line & return E_OK
  *
  * @param tty TTY Context
  * @param line Line buffer to raed into
@@ -154,12 +215,75 @@ error_t tty_read_line(tty_t * tty, tty_line_t * line);
 error_t tty_read_line_async(tty_t * tty, tty_line_t * line);
 
 /**
- * Write line (@ref tty_flag_t)
+ * Write line to TTY output (@ref tty_flag_t)
  *
  * @param tty TTY Context
  * @param line Line buffer to write from
  */
 error_t tty_write_line(tty_t * tty, tty_line_t * line);
+
+/**
+ * Write formatted line to TTY output
+ *
+ * @param tty TTY Context
+ * @param fmt Format string
+ * @param ... Format arguments
+ */
+error_t tty_write_fmt(tty_t * tty, const char * fmt, ...);
+
+/**
+ * Handler for custom CSI escape sequences
+ * Called for unexpected chars after CSI - Control Sequence Introducer ('ESC [')
+ *
+ * @note Defined as weak in tty.c
+ * @note TTY_LINE_INPUT_CUSTOM_STATE define can be used to add custom states to
+ *       tty line input state machine
+ * @note For the duration of sequence parsing, the line->input.state must be
+ *       TTY_LINE_INPUT_STATE_CSI
+ * @note Must set line input state to TTY_LINE_INPUT_STATE_DEFAULT when done parsing
+ *
+ * @param tty  TTY Context
+ * @param line Line buffer
+ * @param c    Received char
+ * @return E_AGAIN to wait for next char, E_OK to process current char as normal
+ */
+error_t tty_process_ansi_csi_custom(tty_t * tty, tty_line_t * line, char c);
+
+/**
+ * Handler for custom DCS escape sequences
+ * Called for unexpected chars after DCS - Device Control String ('ESC P')
+ *
+ * @note Defined as weak in tty.c
+ * @note TTY_LINE_INPUT_CUSTOM_STATE define can be used to add custom states to
+ *       tty line input state machine
+ * @note For the duration of sequence parsing, the line->input.state must be
+ *       TTY_LINE_INPUT_STATE_DCS
+ * @note Must set line input state to TTY_LINE_INPUT_STATE_DEFAULT when done parsing
+ *
+ * @param tty  TTY Context
+ * @param line Line buffer
+ * @param c    Received char
+ * @return E_AGAIN to wait for next char, E_OK to process current char as normal
+ */
+error_t tty_process_ansi_dcs_custom(tty_t * tty, tty_line_t * line, char c);
+
+/**
+ * Handler for custom OSC escape sequences
+ * Called for unexpected chars after OSC - Operating System Command ('ESC ]')
+ *
+ * @note Defined as weak in tty.c
+ * @note TTY_LINE_INPUT_CUSTOM_STATE define can be used to add custom states to
+ *       tty line input state machine
+ * @note For the duration of sequence parsing, the line->input.state must be
+ *       TTY_LINE_INPUT_STATE_OSC
+ * @note Must set line input state to TTY_LINE_INPUT_STATE_DEFAULT when done parsing
+ *
+ * @param tty  TTY Context
+ * @param line Line buffer
+ * @param c    Received char
+ * @return E_AGAIN to wait for next char, E_OK to process current char as normal
+ */
+error_t tty_process_ansi_osc_custom(tty_t * tty, tty_line_t * line, char c);
 
 #ifdef __cplusplus
 }
